09a2eed97002a59e36a700c8496ce553abb470a2
Gulyaev <m.gulyaev@corp.vk.com>
2020-04-03 03:11:49 +0300

    add pronunciation for words

--- app/src/main/java/com/mvwords/categories/WordCategoryHolder.kt
+++ app/src/main/java/com/mvwords/categories/WordCategoryHolder.kt
@@ -1,15 +1,25 @@
 package com.mvwords.categories
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.mvwords.R
+import com.mvwords.utils.SpeechHelper
 import core.Word
 
 class WordCategoryHolder(view: View): RecyclerView.ViewHolder(view) {
 
     private val title: TextView = view.findViewById(R.id.title)
     private val subtitle: TextView = view.findViewById(R.id.subtitle)
+
+    init {
+        view.findViewById<ImageView>(R.id.voice).apply {
+            setOnClickListener {
+                SpeechHelper.speak(view.context, title.text)
+            }
+        }
+    }
 
     fun bind(word: Word) {
         title.text = word.english
---
+++ app/src/main/java/com/mvwords/utils/SpeechHelper.kt
@@ -0,0 +1,25 @@
+package com.mvwords.utils
+
+import android.content.Context
+import android.speech.tts.TextToSpeech
+import java.util.*
+
+object SpeechHelper {
+
+    private var textToSpeech: TextToSpeech? = null
+    private val words = PriorityQueue<String>()
+    private val listener = object: TextToSpeech.OnInitListener {
+        override fun onInit(status: Int) {
+            val word = words.poll() ?: return
+            textToSpeech?.language = Locale.ENGLISH
+            textToSpeech?.speak(word, TextToSpeech.QUEUE_ADD, null, null)
+            textToSpeech = null
+        }
+    }
+
+    fun speak(context: Context, word: CharSequence) {
+        textToSpeech = TextToSpeech(context, listener)
+        words.add(word.toString())
+    }
+
+}
