7bbc15ab3097c7d896276408ef4717987dc1f212
Gulyaev <m.gulyaev@corp.vk.com>
2020-04-11 18:54:41 +0300

    add buy buttons bg, full screen mode

--- app/src/main/java/com/helpfulproduction/mywords/PromoFragment.kt
+++ app/src/main/java/com/helpfulproduction/mywords/PromoFragment.kt
@@ -8,8 +8,9 @@ import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.helpfulproduction.mywords.utils.Navigator
 
-
 class PromoFragment: Fragment() {
+
+    private var flags: Int? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_promo, container, false)
@@ -18,7 +19,27 @@ class PromoFragment: Fragment() {
                 Navigator.onBackPressed()
             }
         }
+
+        activity?.window?.decorView?.systemUiVisibility?.let {
+            makeFullScreen(it)
+        }
+
         return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        resetFlags()
+    }
+
+    private fun makeFullScreen(currentFlags: Int) {
+        activity?.window?.decorView?.systemUiVisibility = currentFlags or fullScreenFlags
+    }
+
+    private fun resetFlags() {
+        activity?.window?.decorView?.systemUiVisibility?.let { flags ->
+            activity?.window?.decorView?.systemUiVisibility = flags xor fullScreenFlags
+        }
     }
 
     class Builder {
@@ -29,6 +50,10 @@ class PromoFragment: Fragment() {
 
     companion object {
         val TAG = PromoFragment::class.java.simpleName
+        private const val fullScreenFlags = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or  View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 
 }
