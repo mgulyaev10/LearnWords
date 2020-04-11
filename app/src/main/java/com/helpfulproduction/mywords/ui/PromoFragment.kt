a9f3d862a25c427056ee413e74162e2a98988395
Gulyaev <m.gulyaev@corp.vk.com>
2020-04-11 22:02:43 +0300

    remove full screen

--- app/src/main/java/com/helpfulproduction/mywords/ui/PromoFragment.kt
+++ app/src/main/java/com/helpfulproduction/mywords/ui/PromoFragment.kt
@@ -11,8 +11,6 @@ import com.helpfulproduction.mywords.utils.Navigator
 
 class PromoFragment: Fragment() {
 
-    private var flags: Int? = null
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_promo, container, false)
         view.findViewById<ImageView>(R.id.close).apply {
@@ -21,26 +19,7 @@ class PromoFragment: Fragment() {
             }
         }
 
-        activity?.window?.decorView?.systemUiVisibility?.let {
-            makeFullScreen(it)
-        }
-
         return view
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        resetFlags()
-    }
-
-    private fun makeFullScreen(currentFlags: Int) {
-        activity?.window?.decorView?.systemUiVisibility = currentFlags or fullScreenFlags
-    }
-
-    private fun resetFlags() {
-        activity?.window?.decorView?.systemUiVisibility?.let { flags ->
-            activity?.window?.decorView?.systemUiVisibility = flags xor fullScreenFlags
-        }
     }
 
     class Builder {
@@ -51,10 +30,6 @@ class PromoFragment: Fragment() {
 
     companion object {
         val TAG = PromoFragment::class.java.simpleName
-        private const val fullScreenFlags = (View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or  View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_FULLSCREEN)
     }
 
 }
